6c0c8362a07109038bc608ba1707dc45590d5bf0
yajiiiii <ijayjaculbe0939@gmail.com>
2024-06-04 18:21:48 +0800

    3page only working not yet totally done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.car.ui.lib)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
